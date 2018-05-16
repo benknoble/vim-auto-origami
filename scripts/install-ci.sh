74532bc9a5f9db8bf7e2d214b263d19ad4d4db11
David Ben Knoble <ben.knoble@gmail.com>
2018-05-16 17:03:29 -0400

    Use newer vim version on travis
    See https://whileimautomaton.net/2018/02/27002447

--- scripts/install-ci.sh
+++ scripts/install-ci.sh
@@ -5,6 +5,14 @@ set -euo pipefail
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}")" && pwd )"
 plugin_dir="$( dirname "$dir" )"
 
+install_vim8() {
+  curl https://raw.githubusercontent.com/kana/vim-version-manager/master/bin/vvm | python - setup; true
+  source ~/.vvm/etc/login
+  vvm update_itself
+  vvm use vimorg--v8.0.1529 --install --with-features=huge
+  rvm use 2.5.0 --install --binary --fuzzy
+}
+
 install_vader() {
   git clone https://github.com/junegunn/vader.vim.git "$plugin_dir/vader"
 }
