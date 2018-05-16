73dbd0e2247fcb4c1c7b71992a3e3de9f9e6be79
David Ben Knoble <ben.knoble@gmail.com>
2018-05-16 17:42:01 -0400

    Try vim8 package

--- scripts/install-ci.sh
+++ scripts/install-ci.sh
@@ -5,16 +5,7 @@ set -euo pipefail
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}")" && pwd )"
 plugin_dir="$( dirname "$dir" )"
 
-get_vvm() {
-  url='https://raw.githubusercontent.com/kana/vim-version-manager/master/bin/vvm'
-  curl "$url" | python - setup
-}
-
 install_vim8() {
-  get_vvm || true
-  source ~/.vvm/etc/login
-  vvm update_itself
-  vvm use vimorg--v8.0.1529 --install --with-features=huge
   # check for version 8
   vim --version | grep -q 'IMproved 8'
 }
