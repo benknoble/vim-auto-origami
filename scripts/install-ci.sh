bd2f03c374cb072ff1df4b8056e7b3684e5d86bc
D. Ben Knoble <ben.knoble+github@gmail.com>
2019-08-14 09:32:39 -0400

    ci: correct test of OS

--- scripts/install-ci.sh
+++ scripts/install-ci.sh
@@ -6,7 +6,7 @@ dir="$( cd "$( dirname "${BASH_SOURCE[0]}")" && pwd )"
 plugin_dir="$( dirname "$dir" )"
 
 install_vim8() {
-  if [[ "$(uname)" == osx ]]; then
+  if [[ "$(uname)" = Darwin ]]; then
     brew update && brew install vim
   else
     sudo add-apt-repository ppa:jonathonf/vim
